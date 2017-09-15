66fae8f4f94e55aa5416016ab351d3c9a12ffe0c
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-09-14 21:34:45 -0400

    Angular 4 Simple Examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+//  templateUrl: './app.component.html',
+  template: `
+
+  <h1>Hey Guys</h1>
+  <p>How are you doing?</p>
+  `,
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
 }
